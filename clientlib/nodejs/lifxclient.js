0b2162b88df297c8d742fb718aa2faa65b155fd4
Simon Fisher <simfish85@googlemail.com>
2017-03-03 15:06:56 +0000

    Update lifxclient.js

--- clientlib/nodejs/lifxclient.js
+++ clientlib/nodejs/lifxclient.js
@@ -21,78 +21,167 @@ function listenToButton(bdAddr) {
 	        switch (bdAddr) {
                   case '80:e4:da:71:c9:55': // Kitchen Flic
                     console.log("Kitchen " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago");
-										var light = lifxClient.light('Kitchen Back');
 										if (clickType == 'ButtonSingleClick') {
-											light.getPower(function(error,data) {
-												if (data == 0) {
-													light.on(1000);
-													lifxClient.light('Kitchen Front').on(1000);
-												}
-												else {
-													light.off(1000);
-													lifxClient.light('Kitchen Front').off(1000);
-												}
-											});
+											var light = lifxClient.light('Kitchen Back');
+											if (light) {
+												light.getPower(function(error,data) {
+													if (data == 0) {
+														light.on(1000);
+														light = lifxClient.light('Kitchen Front');
+														if (light) {
+															light.on(1000);
+														}
+														else {
+															console.log("Kitchen Front could not be found");
+														}
+													}
+													else {
+														light.off(1000);
+														light = lifxClient.light('Kitchen Front');
+														if (light) {
+															light.off(1000);
+														}
+														else {
+															console.log("Kitchen Front could not be found");
+														}
+													}
+												});
+											}
+											else {
+												console.log("Kitchen Back could not be found");
+											}
+										}
+										if (clickType == 'ButtonHold') {
+											var light = lifxClient.light('Garage');
+											if (light) {
+												light.getPower(function(error,data) {
+													if (data == 0) {
+														light.on(1000);
+													}
+													else {
+														light.off(1000);
+													}
+												});
+											}
+											else {
+												console.log("Garage could not be found");
+											}
 										}
 										break;
                   case '80:e4:da:71:a1:bd': // Playroom Flic
                     console.log("Playroom " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago");
                     var light = lifxClient.light('Playroom');
-										if (clickType == 'ButtonSingleClick') {
-											light.color(Math.floor(Math.random() * 361), 80, 70, 3500, 1000);
-											light.getPower(function(error,data) {
-												if (data == 0) {
-													light.on();
-												}
-											});
-										}
-                    if (clickType == 'ButtonDoubleClick') {
-											light.getState(function(error,data) {
-												if (data.color.kelvin == 3000) {
-													light.getPower(function(error,data) {
-														if (data == 0) {
-															light.on();
+										if (light) {
+											if (clickType == 'ButtonSingleClick') {
+												light.color(Math.floor(Math.random() * 361), 80, 70, 3500, 1000);
+												light.getPower(function(error,data) {
+													if (data == 0) {
+														light.on();
+													}
+												});
+											}
+	                    if (clickType == 'ButtonDoubleClick') {
+												light.getState(function(error,data) {
+													if (data) {
+														if (data.color.kelvin == 3000) {
+															light.getPower(function(error,data) {
+																if (data == 0) {
+																	light.on();
+																}
+																else {
+																	light.off();
+																}
+															});
 														}
 														else {
-															light.off();
+															light.color(0,0, 70, 3000, 1000);
 														}
-													});
-												}
-												else {
-													light.color(0,0, 70, 3000, 1000);
-												}
-											});
-                    }
+													}
+													else {
+														console.log("Data not available in playroom");
+													}
+												});
+	                    }
+										}
+										else {
+											console.log("Playroom could not be found");
+										}
                     break;
                   case '80:e4:da:71:af:e5': // Upstairs Flic
                     console.log("Upstairs " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago");
 										if (clickType == 'ButtonSingleClick') {
 											var light = lifxClient.light('Upstairs Landing');
-											light.getPower(function(error,data) {
-											  if (data == 0) {
-													light.on(1000);
-												}
-												else {
-													light.off(1000);
-												}
-											});
+											if (light) {
+												light.getPower(function(error,data) {
+												  if (data == 0) {
+														light.on(1000);
+													}
+													else {
+														light.off(1000);
+													}
+												});
+											}
+											else {
+												console.log("Upstairs Landing could not be found");
+											}
 										}
 										if (clickType == 'ButtonHold') {
 											var light = lifxClient.light('Kitchen Back');
-											light.getPower(function(error,data) {
-												if (data == 0) {
-													light.on(1000);
-													lifxClient.light('Kitchen Front').on(1000);
-													lifxClient.light('Living Room Stand Back').on(1000);
-													lifxClient.light('Living Room Stand Front').on(1000);
-												}
-												else {
-													light.off(1000);
-													lifxClient.light('Kitchen Front').off(1000);
-													lifxClient.light('Living Room Stand Back').off(1000);
-													lifxClient.light('Living Room Stand Front').off(1000);
-												}
-											});
+											if (light) {
+												light.getPower(function(error,data) {
+													if (data == 0) {
+														light.on(1000);
+														light = lifxClient.light('Kitchen Front');
+														if (light) {
+															light.on(1000);
+														}
+														else {
+															console.log("Kitchen Front could not be found");
+														}
+														light = lifxClient.light('Living Room Stand Back');
+														if (light) {
+															light.on(1000);
+														}
+														else {
+															console.log("Living Room Stand Back could not be found");
+														}
+														light = lifxClient.light('Living Room Stand Front');
+														if (light) {
+															light.on(1000);
+														}
+														else {
+															console.log("Living Room Stand Front could not be found");
+														}
+													}
+													else {
+														light.off(1000);
+														light = lifxClient.light('Kitchen Front');
+														if (light) {
+															light.off(1000);
+														}
+														else {
+															console.log("Kitchen Front could not be found");
+														}
+														light = lifxClient.light('Living Room Stand Back');
+														if (light) {
+															light.off(1000);
+														}
+														else {
+															console.log("Living Room Stand Back could not be found");
+														}
+														light = lifxClient.light('Living Room Stand Front');
+														if (light) {
+															light.off(1000);
+														}
+														else {
+															console.log("Living Room Stand Front could not be found");
+														}
+													}
+												});
+											}
+											else {
+												console.log("Kitchen Back could not be found");
+											}
 										}
                     break;
                   default:
--- start.sh
+++ start.sh
@@ -44,9 +44,9 @@ while getopts "hc" opt; do
 done
 
 # Kill any running server processes
-if ps aux | grep '[f]licd' &> /dev/null; then
-    sudo kill $(ps aux | grep '[f]licd' | awk '{print $2}');
-fi
+#if ps aux | grep '[f]licd' &> /dev/null; then
+#    sudo kill $(ps aux | grep '[f]licd' | awk '{print $2}');
+#fi
 
 # Kill any running client processes
 if ps aux | grep 'lifxclient' &> /dev/null; then
@@ -61,18 +61,18 @@ fi
 sleep 3
 
 # Start server in separate terminal - supports ubuntu for testing purposes
-if [[ $(arch) == "x86_64" ]]; then
-     pushd bin/x86_64 &> /dev/null
-     gnome-terminal -e 'sudo ./flicd -f flic.sqlite3'
-     popd &> /dev/null
-elif [[ $(arch) == "armv7l" ]]; then
-     pushd bin/armv6l &> /dev/null
-     lxterminal -e 'sudo ./flicd -f flic.sqlite3'
-     popd &> /dev/null
-else
-    echo "Unsupported OS"
-    exit
-fi
+#if [[ $(arch) == "x86_64" ]]; then
+#     pushd bin/x86_64 &> /dev/null
+#     gnome-terminal -e 'sudo ./flicd -f flic.sqlite3'
+#     popd &> /dev/null
+#elif [[ $(arch) == "armv7l" ]]; then
+#     pushd bin/armv6l &> /dev/null
+#     lxterminal -e 'sudo ./flicd -f flic.sqlite3'
+#     popd &> /dev/null
+#else
+#    echo "Unsupported OS"
+#    exit
+#fi
 
 echo "The server has been launched in another terminal."
 
